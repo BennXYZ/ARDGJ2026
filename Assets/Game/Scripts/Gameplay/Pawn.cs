62b3da2fb3f4032e9e712b27c182144c85690f9b
Benedikt Zöller <benedikt.zoeller@16-tons.com>
2026-08-22 18:00:11 +0200

    Implement Failed Move Animation

--- Assets/Game/Scripts/Gameplay/Pawn.cs
+++ Assets/Game/Scripts/Gameplay/Pawn.cs
@@ -32,35 +32,54 @@ namespace ArdJam2026.Gameplay
         public void Move(Vector2Int direction)
         {
             Vector2Int newPosition = Location + direction;
+            Vector3 startPosition = Room.Level.GetCellCenterWorld((Vector3Int)Location);
+            Vector3 endPosition = Room.Level.GetCellCenterWorld((Vector3Int)newPosition);
+            string animationName = "Possessed_Idle";
+            if (newPosition.x > Location.x)
+                animationName = "Move_Right";
+            if (newPosition.x < Location.x)
+                animationName = "Move_Left";
+            if (newPosition.y > Location.y)
+                animationName = "Move_Up";
+            if (newPosition.y < Location.y)
+                animationName = "Move_Down";
+
             GameplayTile tile = Room.GetTile(newPosition);
             if (tile && tile.Collides)
             {
                 MoveCount = 0;
+                PlayBlockedMoveAnimation(animationName, startPosition, endPosition);
                 return;
             }
 
             if (Room.TryGetColliderAt(newPosition, out ICollider collider) && collider.IsColliding)
             {
                 if (collider.IsStatic)
+                {
                     MoveCount = 0;
+                    PlayBlockedMoveAnimation(animationName, startPosition, endPosition);
+                }
                 return;
             }
 
             MoveCount--;
 
-            Vector3 startPosition = Room.Level.GetCellCenterWorld((Vector3Int)Location);
-            Vector3 endPosition = Room.Level.GetCellCenterWorld((Vector3Int)newPosition);
-            if (newPosition.x > Location.x)
-                PlayMoveAnimation("Move_Right", startPosition, endPosition);
-            if (newPosition.x < Location.x)
-                PlayMoveAnimation("Move_Left", startPosition, endPosition);
-            if (newPosition.y > Location.y)
-                PlayMoveAnimation("Move_Up", startPosition, endPosition);
-            if (newPosition.y < Location.y)
-                PlayMoveAnimation("Move_Down", startPosition, endPosition);
+            PlayMoveAnimation(animationName, startPosition, endPosition);
 
             Location = newPosition;
 
+        }
+
+        private void PlayBlockedMoveAnimation(string animationName, Vector3 from, Vector3 to)
+        {
+            transform.DOKill();
+            transform.position = from;
+            to = from + (to - from).normalized * (to - from).magnitude * 0.4f;
+            animations.PlayAnimation(animationName, 0, PlayIdleAnimation, new Animatable.AnimationEvent(
+                () =>
+                {
+                    transform.DOMove(to, 0.15f).SetEase(Ease.Linear).SetLoops(2, LoopType.Yoyo);
+                }, 3));
         }
 
         private void PlayMoveAnimation(string animationName, Vector3 from, Vector3 to)
